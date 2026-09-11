5aef922d652635295ec0edd36345a82aaf822304
tharun <tharuntrendy899@gmail.com>
2026-09-11 16:24:08 +0530

    Fix vite.config.js hrm code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
   server: {
     port: 3000,
-
-    proxy: {
-      '/api': {
-        target: 'https://k12-revenue-app.onrender.com/',
-        changeOrigin: true,
-      },
-    },
-  },
-});
+    hmr: {
+      clientPort: 3000,
+      port: 3000
+    }
+  }
+})
