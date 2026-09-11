0f67f673b67129e996b954c8a80a7bbda42c5a53
tharun <tharuntrendy899@gmail.com>
2026-09-11 21:55:13 +0530

    updated

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -31,7 +31,9 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (email, password) => {
     const res = await api.post("/auth/login", { email, password });
+    console.log(res)
     const { token: newToken, user: newUser } = res.data;
+
     setToken(newToken);
     setUser(newUser);
     localStorage.setItem('k12_token', newToken);
